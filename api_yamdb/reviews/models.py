6831350bf6314a17034f4e5f2d133f921e1b4e4b
Ivan Novikov <warhak511@mail.ru>
2023-05-11 09:50:17 +0300

    Add model UserRoles.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,52 +1,48 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserRoles(models.TextChoices):
+    USER = 'user', _('User')
+    MODERATOR = 'moderator', _('Moderator')
+    ADMIN = 'admin', _('Admin')
 
 
 class User(AbstractUser):
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-
-    ROLES = (
-        (USER, 'Пользователь'),
-        (MODERATOR, 'Модератор'),
-        (ADMIN, 'Админ'),
-    )
-
     username = models.CharField(
-        "Имя пользователя",
+        'Username',
         max_length=settings.LIMIT_USERNAME,
         unique=True,
         error_messages={
-            "unique": "Это имя занято, выберите другое!",
+            'unique': 'This name is taken, please select another!',
         })
 
     email = models.EmailField(
-        verbose_name='Электронная почта',
+        verbose_name='Email',
         unique=True,
         max_length=254,
     )
     bio = models.TextField(
-        verbose_name='Биография',
+        verbose_name='Biography',
         blank=True,
         max_length=250,
     )
     role = models.CharField(
-        verbose_name='Роль',
-        default='user',
-        choices=ROLES,
+        default=UserRoles.USER,
+        choices=UserRoles.choices,
         max_length=12,
     )
     confirmation_code = models.CharField(
-        verbose_name='Код подтверждения',
+        verbose_name='Confirmation code',
         blank=True,
         max_length=50
     )
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
         constraints = [
             models.UniqueConstraint(
                 fields=('username', 'email'),
@@ -58,15 +54,15 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-        return self.role == self.ADMIN or self.is_superuser
+        return self.role == UserRoles.ADMIN or self.is_superuser
 
     @property
     def is_moderator(self):
-        return self.role == self.MODERATOR
+        return self.role == UserRoles.MODERATOR
 
     @property
     def is_user(self):
-        return self.role == self.USER
+        return self.role == UserRoles.USER
 
     def __str__(self):
-        return self.username[:15]
+        return self.username[:settings.STRING_OUTPUT_LENGTH]
