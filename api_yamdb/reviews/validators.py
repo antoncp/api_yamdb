1b5e52af44ba0e730967cfe9f541403529772111
Ivan Novikov <warhak511@mail.ru>
2023-05-15 20:48:42 +0300

    Error commit

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -2,7 +2,7 @@ from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 
 username_validator = RegexValidator(
-    regex=r'^[\w.@+-]+\Z',
+    regex=r'^[\\w.@+-]+\\z',
     message='Username should contain only letters, digits,'
     'and the following characters: @ . + - ',
     code='invalid_username'
